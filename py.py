f9b97621c26b4ee3ad7e9d8c72edfb8b43d9320c
Sweety083 <122953843+Sweety083@users.noreply.github.com>
2025-02-02 20:26:11 +0530

    Add files via upload

--- py.py
+++ py.py
@@ -1,35 +1,3 @@
-# Developing a Simple Webserver.
-## Register number:22009103
-## Developed by:Sabari Akash
-
-# AIM:
-
-Develop a webserver to display about top five web application development frameworks.
-
-# DESIGN STEPS:
-
-## Step 1:
-
-HTML content creation is done
-
-## Step 2:
-
-Design of webserver workflow
-
-## Step 3:
-
-Implementation using Python code
-
-## Step 4:
-
-Serving the HTML pages.
-
-## Step 5:
-
-Testing the webserver
-
-# PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -55,11 +23,3 @@ server_address = ('', 8080)
 httpd = HTTPServer(server_address, HelloHandler)
 print("my webserver is running.....")
 httpd.serve_forever()
-```
-# OUTPUT:
-![output](/output.png)
-![output](/output1.png)
-
-# RESULT:
-
-The program is executed succesfully
